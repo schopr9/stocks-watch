7e2e66fa5d41e3628aaf4256939efd4aeb3ac67f
saurabh <schopr9@uwo.ca>
2020-02-27 17:35:20 -0600

    Stocks watch first commit integrated with finnhub

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch('https://finnhub.io/api/v1/quote?symbol=IVV&token=bpbgne7rh5r9k08n8teg').then(response => response.json())
+      setData(result);
+    }
+    fetchData();
+  }, {})
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Stocks Watch</h1>
       </header>
+      Stock name IVV
+      <br></br>
+      Current Price {data.c}
+      <br></br>
+      opening Price {data.o}
+      <br></br>
+      day heighest {data.h}
+      <br></br>
+      day lowest {data.l}
     </div>
   );
 }
