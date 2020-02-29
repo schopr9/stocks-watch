aa6e758f52428a8ae4ff20bcfca93ac514c18ad0
saurabhchopra <schopr9@uwo.ca>
2020-02-28 22:46:44 -0500

    added icons for all devices

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React, { useState, useEffect }  from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
   const [data, setData] = useState({});
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await fetch('https://finnhub.io/api/v1/quote?symbol=IVV&token=bpbgne7rh5r9k08n8teg').then(response => response.json())
-      setData(result);
-    }
+      try {
+        const result = await fetch(
+          "https://finnhub.io/api/v1/quote?symbol=IVV&token=bpbgne7rh5r9k08n8teg"
+        ).then(response => response.json());
+        setData(result);
+      } catch (e) {
+        setData({
+          c: 301.25,
+          o: 312.25,
+          h: 289.36,
+          l: 125.23
+        });
+      }
+    };
     fetchData();
-  }, {})
+  }, []);
   return (
     <div className="App">
       <header>
