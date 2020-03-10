2cbbea990b287e9bd61983ce454c413862f5aedd
saurabhchopra <schopr9@uwo.ca>
2020-03-10 14:06:11 -0400

    added bottom bar

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
-import RestoreIcon from "@material-ui/icons/Restore";
+import HomeIcon from "@material-ui/icons/Home";
 import FavoriteIcon from "@material-ui/icons/Favorite";
-import LocationOnIcon from "@material-ui/icons/LocationOn";
+import ArrowDownward from "@material-ui/icons/ArrowDownward";
 import "./App.css";
 import { promptUser, deferredInstallPrompt } from "./install";
 
@@ -25,9 +25,9 @@ const useStyles = makeStyles({
 
 function App() {
   const [data, setData] = useState({});
-  const userAgent = navigator.userAgent;
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
+  const totalGainOrLoss = 59 * data.c - (50 * 315 + 9 * 301);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -51,31 +51,31 @@ function App() {
     <div className="App">
       <header>
         <h1>Stocks Watch</h1>
-        {/android/i.test(userAgent) && deferredInstallPrompt && (
-          <button
-            style={{
-              color: "white",
-              padding: "10px",
-              backgroundColor: "brown",
-              marginLeft: "30px"
-            }}
-            onClick={promptUser}
-          >
-            Install
-          </button>
-        )}
       </header>
-      <div className="stock">
-        Stock name IVV
-        <br></br>
-        Current Price {data.c}
-        <br></br>
-        opening Price {data.o}
-        <br></br>
-        day heighest {data.h}
-        <br></br>
-        day lowest {data.l}
-      </div>
+      {value === 0 && (
+        <div className="stock">
+          Stock name IVV
+          <br></br>
+          Current Price {data.c}
+          <br></br>
+          Opening Price {data.o}
+          <br></br>
+          Day highest {data.h}
+          <br></br>
+          Day lowest {data.l}
+        </div>
+      )}
+      {value === 1 && (
+        <div className="stock">
+          Stock name IVV
+          <br></br>
+          Total Invested = {50 * 315 + 9 * 301}
+          <br></br>
+          Total {totalGainOrLoss > 0 ? "gain" : "loss"} ={" "}
+          {totalGainOrLoss.toFixed(2)}
+          <br></br>
+        </div>
+      )}
       <BottomNavigation
         value={value}
         onChange={(event, newValue) => {
@@ -85,19 +85,23 @@ function App() {
         className={classes.root}
       >
         <BottomNavigationAction
-          label="Recents"
-          icon={<RestoreIcon />}
+          label="Home"
+          icon={<HomeIcon />}
           className={classes.button}
         />
         <BottomNavigationAction
-          label="Favorites"
+          label="Holdings"
           icon={<FavoriteIcon />}
           className={classes.button}
         />
-        <BottomNavigationAction
-          label="Nearby"
-          icon={<LocationOnIcon className={classes.button} />}
-        />
+        {deferredInstallPrompt && (
+          <BottomNavigationAction
+            label="Install"
+            icon={<ArrowDownward />}
+            className={classes.button}
+            onClick={promptUser}
+          />
+        )}
       </BottomNavigation>
     </div>
   );
